4376b72b42935a61e1067a02087a14ba47972341
Hariprasaath28092002 <shp28092002@gmail.com>
2026-09-16 22:05:34 +0530

    Fix production CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     "http://localhost:5173",
-    "https://your-actual-portfolio.vercel.app"
+    "https://hariprasaath-portfolio.vercel.app/"
 ]cd bac
     allow_credentials=True,
     allow_methods=["*"],
